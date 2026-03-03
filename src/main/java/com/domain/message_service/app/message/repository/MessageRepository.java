61538ffa3af9bbdd64b69d3e32b84c9c2a9e4562
sougata <sougatadas@cdac.in>
2026-03-03 17:01:13 +0530

    repository method to fetch UUIDs of the room messages.

--- src/main/java/com/domain/message_service/app/message/repository/MessageRepository.java
+++ src/main/java/com/domain/message_service/app/message/repository/MessageRepository.java
@@ -2,6 +2,7 @@ package com.domain.message_service.app.message.repository;
 
 import com.domain.message_service.app.message.entity.MessageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,9 @@ import java.util.UUID;
 public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
     Optional<MessageEntity> findByUuid(UUID uuid);
 
+    @Query("select e from MessageEntity e where e.room.id = :roomId order by e.createdAt desc")
     List<MessageEntity> findByRoomId(Long roomId);
+
+    @Query("select e.uuid from MessageEntity e where e.room.id = :roomId order by e.createdAt desc")
+    List<UUID> getMessagesHash(Long roomId);
 }
