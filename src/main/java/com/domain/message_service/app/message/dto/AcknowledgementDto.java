b8d5e45cb71b504f9bb551dd5df7c8122eac9999
sougata <sougata19582001@gmail.com>
2026-03-30 23:02:56 +0530

    added acknowledgement dto

---
+++ src/main/java/com/domain/message_service/app/message/dto/AcknowledgementDto.java
@@ -0,0 +1,18 @@
+package com.domain.message_service.app.message.dto;
+
+import com.domain.message_service.app.message.enums.Status;
+import lombok.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class AcknowledgementDto {
+    private Map<UUID, List<UUID>> messageMap;
+    private Status status;
+}
